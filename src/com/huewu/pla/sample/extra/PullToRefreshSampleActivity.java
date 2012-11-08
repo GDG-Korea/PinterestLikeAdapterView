34390ae8852a834cba28191f09867eb5e7873c01
Chansuk Yang <huewu@Chansuk-ui-MacBook-Pro.local>
2012-11-09 00:43:14 +0900

    supprot pull-to-refresh

--- src/com/huewu/pla/sample/extra/PullToRefreshSampleActivity.java
+++ src/com/huewu/pla/sample/extra/PullToRefreshSampleActivity.java
@@ -1,93 +1,82 @@
-package com.huewu.pla.sample;
+package com.huewu.pla.sample.extra;
 
 import java.util.Arrays;
 import java.util.Random;
 
+import com.handmark.pulltorefresh.library.PullToRefreshAdapterViewBase;
+import com.huewu.pla.smaple.R;
+import com.huewu.pla.lib.extra.PullToRefreshMultiColumnListView;
+import com.huewu.pla.lib.internal.PLA_AdapterView;
+
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 
-import com.huewu.pla.lib.internal.PLA_AdapterView;
-import com.huewu.pla.sample.extra.PullToRefreshSampleActivity;
-import com.huewu.pla.smaple.R;
-
-public class SampleActivity extends Activity {
-
+public class PullToRefreshSampleActivity extends Activity {
+	
 	private class MySimpleAdapter extends ArrayAdapter<String> {
 
 		public MySimpleAdapter(Context context, int layoutRes) {
 			super(context, layoutRes, android.R.id.text1);
 		}
 	}
-
-	private PLA_AdapterView<ListAdapter> mAdapterView = null;
+	
+	//private PLA_AdapterView<Adapter> mAdapterView = null;
+	private PullToRefreshMultiColumnListView mPullToRefreshView = null;
 	private MySimpleAdapter mAdapter = null;
-
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.act_sample);
+		setContentView(R.layout.act_pull_to_refresh_sample);
 		//mAdapterView = (PLA_AdapterView<Adapter>) findViewById(R.id.list);
-		mAdapterView = (PLA_AdapterView<ListAdapter>) findViewById(R.id.list);
+		mPullToRefreshView = (PullToRefreshMultiColumnListView) findViewById(R.id.list);
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		menu.add(Menu.NONE, 1001, 0, "Load More Contents");
-		menu.add(Menu.NONE, 1002, 0, "Launch Pull-To-Refresh Activity");
+		menu.add("load more contents");
 		return super.onCreateOptionsMenu(menu);
 	}
-
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		int startCount = mAdapter.getCount();
+		for( int i = 0; i < 100; ++i){
+			//generate 100 random items.
 
-		switch(item.getItemId()){
-		case 1001:
-		{
-			int startCount = mAdapter.getCount();
-			for( int i = 0; i < 100; ++i){
-				//generate 100 random items.
+			StringBuilder builder = new StringBuilder();
+			builder.append("Hello!![");
+			builder.append(startCount + i);
+			builder.append("] ");
 
-				StringBuilder builder = new StringBuilder();
-				builder.append("Hello!![");
-				builder.append(startCount + i);
-				builder.append("] ");
-
-				char[] chars = new char[mRand.nextInt(100)];
-				Arrays.fill(chars, '1');
-				builder.append(chars);
-				mAdapter.add(builder.toString());
-			}
+			char[] chars = new char[mRand.nextInt(100)];
+			Arrays.fill(chars, '1');
+			builder.append(chars);
+			mAdapter.add(builder.toString());
 		}
-		break;
-		case 1002:
-		{
-			Intent intent = new Intent(this, PullToRefreshSampleActivity.class);
-			startActivity(intent);
-		}
-		break;
-		}
+		
 		return true;
 	}
-
+	
 	@Override
 	protected void onResume() {
 		super.onResume();
 		initAdapter();
-		mAdapterView.setAdapter(mAdapter);
+		mPullToRefreshView.setAdapter(mAdapter);
 		//mAdapterView.setAdapter(mAdapter);
 	}
 
 	private Random mRand = new Random();
 	private void initAdapter() {
 		mAdapter = new MySimpleAdapter(this, R.layout.item_sample);
-
+		
 		for( int i = 0; i < 100; ++i){
 			//generate 100 random items.
 
@@ -101,7 +90,7 @@ public class SampleActivity extends Activity {
 			builder.append(chars);
 			mAdapter.add(builder.toString());
 		}
-
+		
 	}
 
 }//end of class
